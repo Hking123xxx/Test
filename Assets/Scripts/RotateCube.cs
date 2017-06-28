d8e8880e561e2b9f332dca37d5f2f8a50c204b32
建豪 <472516763@qq.com>
2017-06-28 09:16:59 +0800

    create a test project

---
+++ Assets/Scripts/RotateCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up);
+        Color mColor = new Color(
+            Mathf.Lerp(0.5f, 1f, 0.5f),
+            Mathf.Lerp(0.2f, 1f, 0.3f),
+            Mathf.Lerp(0.1f, 1f, 0.6f));
+        gameObject.GetComponent<Renderer>().material.color = mColor;
+    }
+}
